51857f0d2a7cd2688f2b8802491c21ec40aeaf8e
nilmadhab <nilmadhab1994@gmail.com>
2015-08-30 16:50:57 +0530

    wfeada

--- clean_property_listing.py
+++ clean_property_listing.py
@@ -22,10 +22,29 @@ for row in data :
 	#print file_name
 	#arr.append(file_name)
 	row_id = row[0]
+	document_name = row[1]
 	proprty_type = row[2]
+	#Building Type,Community Name
+	building_type = row[3]
+	storeys = row[4]
+	title = row[5]
+	community_name = row[6] 
+	land_size_length = row[7] 
+	parking_type = row[11]
+
+	document_name = document_name.replace(".html",'')
 	proprty_type = proprty_type.replace("Property Type",'').strip(' \t\n\r')
+	building_type = building_type.replace("Building Type",'').strip(' \t\n\r')
+	community_name = community_name.replace("Community Name",'').strip(' \t\n\r')
+	title = title.replace("Title",'').strip(' \t\n\r')
+	storeys = storeys.replace("Storeys",'').strip(' \t\n\r')
+	parking_type = parking_type.replace("Parking Type",'').strip(' \t\n\r')
+	land_size_length = land_size_length.replace("Land Size",'').strip(' \t\n\r')
 	#print row_id,proprty_type
-	sql =  " UPDATE `property_listing` SET `property_type`='"+ proprty_type + "' WHERE id = "+ str(row_id)+" "
+	sql =  " UPDATE `property_listing` SET `property_type`='"+ proprty_type + "'\
+	,`building_type`='"+ building_type + "',`document_name`='"+ document_name + "',`storeys`='"+ storeys + "' \
+	 ,`title`='"+ title + "',`parking_type`='"+ parking_type + "' \
+	 ,`land_size_length`='"+ land_size_length + "',`community_name`='"+ community_name + "'  WHERE id = "+ str(row_id)+" "
 	print sql
 	try:
 	   # Execute the SQL command
